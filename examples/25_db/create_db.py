6065bffea0e90ae71c4c4b9884dd6c47c6ce35ca
Ivan1998-23 <birjukivan@gmail.com>
2022-10-25 14:31:22 -0500

    Closse cyrs

--- examples/25_db/create_db.py
+++ examples/25_db/create_db.py
@@ -19,15 +19,22 @@ name_db = 'dhcp_snooping.db'
 name_sql = 'dhcp_snooping_schema.sql'
 
 #Получение списка файлов в директории
-def show_filies():
+def show_filies(name_db = None):
     path = os.getcwd()
+    print('-'*40)
     with os.scandir(path) as listOfEntries:
         for entry in listOfEntries:
             # печать всех записей, являющихся файлами
             if entry.is_file():
-                print(' -',entry.name)
+                if name_db == None:
+                    print(' -',entry.name)
+                elif name_db in entry.name:
+                    print(' #',entry.name)
+                else:
+                    print(' -',entry.name)
+    print('-'*40)
 
-def create_db(name_db):
+def create_db():
     #проверка есть ли БД 
     db_exist = os.path.exists(name_db)
     
@@ -36,17 +43,18 @@ def create_db(name_db):
         delet_db = input('Удалить БД (y/n) ? ')
         if delet_db in "yes":  
             path = os.path.join(os.path.abspath(os.path.dirname(__file__)), name_db)
-            os.remove(path)
-            
-            print('-'*40)
-            show_filies()
-            print('-'*40)
+            os.remove(path)  
+            show_filies() 
+        else:  
+            show_filies(name_db) 
     else:
         print('Создаю базу данных...')
-        conn = sqlite3.connect(name_db)
-        
-    
+        conn = sqlite3.connect(name_db) 
+        with open(name_sql, 'r') as f:
+            schema = f.read()
+            conn.executescript(schema)
+        conn.close()
 
 
 if __name__ == '__main__':
-    create_db(name_db)
+    create_db()
